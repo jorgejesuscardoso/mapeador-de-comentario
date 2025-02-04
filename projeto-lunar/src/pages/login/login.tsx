e5e775f34c3ae69d000372212a32cbf6b38d3e29
unknown <tst.jorgecardoso@gmail.com>
2025-02-04 19:30:50 -0300

    sistema de login fake

--- projeto-lunar/src/pages/login/login.tsx
+++ projeto-lunar/src/pages/login/login.tsx
@@ -1,6 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { Container, ContainerFormLogin, ContainerLogin, FormLogin, WelcomeBox } from "./style";
+import { useState } from "react";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState('');
+    const [p, setP] = useState('');
+
+    const fakelogin = (e) => {
+        const fakeUser = 'AnnaFr4nca';
+        const fakePassword = 'vizinha';
+
+        if (user === fakeUser && p === fakePassword) {
+            
+            navigate('/home');
+        }  else {
+            alert('Usuário ou senha incorretos!');
+        }
+    }
     return (
         <Container>
             <WelcomeBox>
@@ -14,9 +31,25 @@ const Login = () => {
                 <ContainerFormLogin>
                     <h1>Login</h1>
                     <FormLogin>
-                        <input type="text" placeholder="Usuário" />
-                        <input type="password" placeholder="Senha" />
-                        <button>Entrar</button>
+                        <input 
+                            type="text"
+                            placeholder="Usuário"
+                            value={user}
+                            onChange={(e) => setUser(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            value={p}
+                            onChange={(e) => setP(e.target.value)}
+                        />
+                        <button
+                            onClick={(e) => fakelogin(
+                                e.preventDefault()
+                            )}
+                        >
+                            Entrar
+                        </button>
                     </FormLogin>
                     <span>
                         Aréa reservada à administradores. <br/> Caso não seja um, por favor, retorne a página inicial.
