8654766e0b4230efd4a7ae45711e80cf5cf44763
unknown <tst.jorgecardoso@gmail.com>
2025-02-05 00:56:40 -0300

    mudança no video de login

--- projeto-lunar/src/component/animate/login/modal_login.tsx
+++ projeto-lunar/src/component/animate/login/modal_login.tsx
@@ -3,37 +3,35 @@ import { ContainerAnimated } from "./style"
 import { useNavigate } from "react-router-dom";
 
 const AnimatedLogin = () => {
-    
-    const [index, setIndex] = useState(0);
     const [gifs, setGifs] = useState("");
     const navigate = useNavigate();
 
     const LoginAnimated = () => {
-        const frases = [
-            "Esquentando os motores",
-            "Decolando",
-            "Em órbita",
-            "Chegando à Lua",
-            "Pousando"
-        ];
+        // const frases = [
+        //     "Esquentando os motores",
+        //     "Decolando",
+        //     "Em órbita",
+        //     "Chegando à Lua",
+        //     "Pousando"
+        // ];
     
         useEffect(() => {
-            if (index >= frases.length - 1) {
-                setGifs("end-trip.mp4");
-                setTimeout(() => navigate('/home'), 2000); // Aguarda 1s antes de redirecionar
-                return; // Para de executar o efeito
-            }
+            
+            setGifs("end-trip.mp4");
+            setTimeout(() => navigate('/home'), 7000); // Aguarda 1s antes de redirecionar
+            return; // Para de executar o efeito
+            
             
 
-            const intervalo = setInterval(() => {
-                setIndex((prevIndex) => prevIndex + 1);
-            }, 1000); // Troca a frase a cada 1s
+            // const intervalo = setInterval(() => {
+            //     setIndex((prevIndex) => prevIndex + 1);
+            // }, 1000); // Troca a frase a cada 1s
     
-            return () => {
-                clearInterval(intervalo); // Limpa o intervalo ao desmontar ou mudar o index
-            };
+            // return () => {
+            //     clearInterval(intervalo); // Limpa o intervalo ao desmontar ou mudar o index
+            // };
 
-        }, [index, navigate, frases.length]);
+        }, []);
     
         return <span>
             <video
@@ -43,7 +41,6 @@ const AnimatedLogin = () => {
                 muted
                 style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, objectFit: 'cover' }}
             />
-            {frases[index]}
         </span>;
     };
 
