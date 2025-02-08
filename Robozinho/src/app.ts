9702bf83e245689bcda4942adc073cabdaddaf32
unknown <tst.jorgecardoso@gmail.com>
2025-02-08 03:08:01 -0300

    adicionando rotas GET e layouts membros

--- Robozinho/src/app.ts
+++ Robozinho/src/app.ts
@@ -7,7 +7,13 @@ import Routes from './routes';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: 'GET, POST, PUT, DELETE',
+        allowedHeaders: 'Content-Type, Authorization, Origin, X-Requested-With, Accept',
+    }
+));
 app.use(express.json());
 
 app.use(Routes);
