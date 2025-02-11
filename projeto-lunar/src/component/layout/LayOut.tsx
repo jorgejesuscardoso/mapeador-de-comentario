47895d338ce7587c31ecb92fa1425b88727912fa
unknown <tst.jorgecardoso@gmail.com>
2025-02-10 23:22:52 -0300

    modificando o robozinho

--- projeto-lunar/src/component/layout/LayOut.tsx
+++ projeto-lunar/src/component/layout/LayOut.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom"
 import HeaderLunar from "../header/header"
 import { LayOutContainer } from "./style"
+import Footer from "../footer/Footer"
 
 const LayOut = () => {
   return (
     <LayOutContainer>
       <HeaderLunar />
       <Outlet />
+      <Footer />
     </LayOutContainer>
   )
 }
