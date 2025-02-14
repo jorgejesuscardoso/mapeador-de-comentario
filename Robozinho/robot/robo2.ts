e8b897b8a1358e724e4407c7e2ef6a1ae7249312
unknown <tst.jorgecardoso@gmail.com>
2025-02-13 23:06:12 -0300

    fix tsc

--- Robozinho/robot/robo2.ts
+++ Robozinho/robot/robo2.ts
@@ -6,7 +6,6 @@ export const Robozinho2 = async (wUser: string, wUrl: string, click: number) => 
     async function Robot() {
         const browser = await chromium.launch({
             headless: true,
-            executablePath: '/usr/bin/google-chrome-stable',
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });        
 
