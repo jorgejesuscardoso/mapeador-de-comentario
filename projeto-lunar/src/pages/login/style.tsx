f698e5af6542dacc0b037f0ff913fee69edc71e6
unknown <tst.jorgecardoso@gmail.com>
2025-02-14 17:55:08 -0300

    responsive login

--- projeto-lunar/src/pages/login/style.tsx
+++ projeto-lunar/src/pages/login/style.tsx
@@ -7,6 +7,10 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: #fff;
+  
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 export const ContainerLogin = styled.div`
@@ -16,6 +20,14 @@ export const ContainerLogin = styled.div`
   width: 40vw;
   height: 100%;
   background-color: #fff;
+
+  @media (max-width: 1024px) {
+    width: 60vw;
+  }
+  
+  @media (max-width: 768px) {
+    width: 100vw;
+  }
 `;
 
 export const ContainerFormLogin = styled.div`
@@ -27,97 +39,104 @@ export const ContainerFormLogin = styled.div`
   height: 100%;
   background-color: #f5f5f5;
 
-    h1 {
-        font-size: 2rem;
-        margin-bottom: 10px;
-    }
-  
-    span {
-        text-align: center;
-        font-size: 13px;
-        width: 100%;
-    }
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 10px;
+  }
+
+  span {
+    text-align: center;
+    font-size: 13px;
+    width: 100%;
+  }
 `;
 
 export const FormLogin = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 40%;
+  height: 50%;
+  background-color: #f5f5f5;
+  gap: 10px;
+
+  input {
+    font-size: 0.8rem;
+    padding: 10px;
+    width: 100%;
+    height: 40px;
+    border: 1px solid #000;
+    border-radius: 7px;
+  }
+
+  button {
+    width: 100%;
+    height: 40px;
+    background-color: #000;
+    color: #fff;
+    border: none;
+    border-radius: 7px;
+    cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+export const WelcomeBox = styled.div`
+  background-image: url("oficial-img.jpg");
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 70vw;
+  height: 100%;
+  background-color: #fff;
+  font-size: 1.5rem;
+  gap: 10px;
+
+  div {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 40%;
-    height: 50%;
-    background-color: #f5f5f5;
-    gap: 10px;
-
-    input {
-        font-size: 0.8rem;
-        padding: 10px;
-        width: 100%;
-        height: 40px;
-        border: 1px solid #000;
-        border-radius: 7px;
-    }
-    
-    button {
-        width: 100%;
-        height: 40px;
-        background-color: #000;
-        color: #fff;
-        border: none;
-        border-radius: 7px;
-        cursor: pointer;
-    }
-
-`;
-
-export const WelcomeBox = styled.div`
-    background-image: url("oficial-img.jpg");
-    background-size: cover;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 70vw;
+    width: 100%;
     height: 100%;
-    background-color: #fff;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  h1 {
+    font-size: 2rem;
+    color: #fff;
+  }
+
+  h2 {
     font-size: 1.5rem;
-    gap: 10px;
+    margin-top: -10px;
+    color: #fff;
+  }
 
-    div {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.5);
-    }
+  span {
+    text-align: center;
+    font-size: 13px;
+    width: 100%;
+  }
 
-    h1 {
-        font-size: 2rem;
-        color: #fff;
-    }
-
-    h2 {
-        font-size: 1.5rem;
-        margin-top: -10px;
-        color: #fff;
-    }
-
-    span {
-        text-align: center;
-        font-size: 13px;
-        width: 100%;
-    }
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 export const SpanAuthenticating = styled.span`
-    font-size: 0.7rem !important;
-    color: red;    
-    margin-top: 10px;
-`;
-export const SpanLogging = styled.span`
-    font-size: 0.7rem !important;
-    color: green;    
-    margin-top: 10px;
+  font-size: 0.7rem !important;
+  color: red;    
+  margin-top: 10px;
 `;
 
+export const SpanLogging = styled.span`
+  font-size: 0.7rem !important;
+  color: green;    
+  margin-top: 10px;
+`;
