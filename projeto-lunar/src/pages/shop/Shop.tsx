f0911064cf910cf7ba8a8f2d5270ba90e2bde814
unknown <tst.jorgecardoso@gmail.com>
2025-03-01 13:50:26 -0300

    decimo 5 commit

--- projeto-lunar/src/pages/shop/Shop.tsx
+++ projeto-lunar/src/pages/shop/Shop.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { GetFromLocalStorage } from "../../utils/localstorage";
+
 const Shop = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getLocalStorage = GetFromLocalStorage('user');
+        if (getLocalStorage === null) {
+            navigate('/');
+            return;
+        }
+    }, []);
+    
     return (
         <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
            
