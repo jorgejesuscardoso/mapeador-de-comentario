a0c1a97096da599497f68588c60769db11483420
unknown <tst.jorgecardoso@gmail.com>
2025-06-11 20:44:15 -0300

    obj

--- projeto-lunar/src/API/UrlBase.ts
+++ projeto-lunar/src/API/UrlBase.ts
@@ -1,6 +1,6 @@
 export const UrlBase = {
     prod: 'https://projlunar.onrender.com',
     dev: 'http://localhost:6060',
-    ngrok: 'https://eda4-170-84-225-240.ngrok-free.app',
+    ngrok: 'https://7dd1-170-84-225-203.ngrok-free.app',
     rail: 'https://mapeador-de-comentario-production.up.railway.app',
 };
