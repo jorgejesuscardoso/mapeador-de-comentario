f65d52e2f5d30edbbac883830e2ec14a41e68331
unknown <tst.jorgecardoso@gmail.com>
2025-06-22 01:13:54 -0300

    obj

--- lunar_v3/src/API/Base.url.ts
+++ lunar_v3/src/API/Base.url.ts
@@ -1,5 +1,5 @@
 export const UrlBase = {
-    prod: 'https://map-v3.onrender.com',
+    prod: 'https://gc92plspo3.execute-api.sa-east-1.amazonaws.com/dev',
     dev: 'http://localhost:6060',
     ngrok: 'https://9d85-170-84-225-203.ngrok-free.app',
     rail: 'https://mapeador-de-comentario-production.up.railway.app',
