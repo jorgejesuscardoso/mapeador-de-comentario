c81220b82a44380984a1bcb42cd74e088d1aa313
unknown <tst.jorgecardoso@gmail.com>
2025-06-21 21:11:19 -0300

    obj

--- Robozinho/src/routes/index.ts
+++ Robozinho/src/routes/index.ts
@@ -1,13 +1,7 @@
-import express, { Request, Response } from 'express';
-import Auths from '../routes/auth';
+import express from 'express';
 import bot from './robot';
-import User from './users';
-import Book from './book';
 
 const Routes = express.Router();
-Routes.use(Auths);
 Routes.use(bot);
-Routes.use(User);
-Routes.use(Book);
 
 export default Routes;
