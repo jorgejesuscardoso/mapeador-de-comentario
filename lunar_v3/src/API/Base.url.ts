13384ff1624e575edb7370be893b08dd4601a71f
unknown <tst.jorgecardoso@gmail.com>
2025-06-23 19:47:44 -0300

    render

--- lunar_v3/src/API/Base.url.ts
+++ lunar_v3/src/API/Base.url.ts
@@ -1,4 +1,5 @@
 export const UrlBase = {
+    render: 'https://map-v3.onrender.com',
     prod: 'https://gc92plspo3.execute-api.sa-east-1.amazonaws.com/dev',
     dev: 'http://localhost:6060',
     ngrok: 'https://9d85-170-84-225-203.ngrok-free.app',
