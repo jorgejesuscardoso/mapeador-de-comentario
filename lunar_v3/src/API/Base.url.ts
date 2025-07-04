3ea1075ffb4b1de70e3bb8d78bc5e7d71b86eb34
unknown <tst.jorgecardoso@gmail.com>
2025-07-04 20:51:52 -0300

    change api

--- lunar_v3/src/API/Base.url.ts
+++ lunar_v3/src/API/Base.url.ts
@@ -1,6 +1,6 @@
 export const UrlBase = {
-    render: 'https://map-v3.onrender.com',
-    prod: 'https://gc92plspo3.execute-api.sa-east-1.amazonaws.com/dev',
+    prod: 'https://map-v3.onrender.com',
+    aws: 'https://gc92plspo3.execute-api.sa-east-1.amazonaws.com/dev',
     dev: 'http://localhost:6060',
     ngrok: 'https://9d85-170-84-225-203.ngrok-free.app',
     rail: 'https://mapeador-de-comentario-production.up.railway.app',
