fd19e8a5fa067df77d4402fd1d2610a198cfea6e
unknown <tst.jorgecardoso@gmail.com>
2025-07-15 01:57:47 -0300

    melhorado a pagina de perfil

--- lunar_v3/src/pages/members/Members.vue
+++ lunar_v3/src/pages/members/Members.vue
@@ -1,6 +1,7 @@
 
 <script setup lang="ts">
 import { getUser, getUserById, getUserWtpd } from '@/API/UserApi'
+import Lucide from '@/base/lucide/Lucide.vue'
 import { ref, onMounted } from 'vue'
 
 const data = ref([])
@@ -19,11 +20,11 @@ onMounted(async () => {
 
 
 <template>
-  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-11/12 mx-auto mt-10">
+  <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 w-full mx-auto mt-10">
     <div
       v-for="member in data"
       :key="member.user"
-      class="bg-white border border-purple-200 rounded-xl shadow-md p-5 hover:shadow-xl transition-all flex flex-col items-center text-center space-y-6 relative"
+      class="bg-white border border-purple-200 rounded-xl shadow-md p-2 hover:shadow-xl transition-all flex flex-col items-center text-center bg-red relative"
     >
       <!-- Avatar -->
       <div
@@ -32,15 +33,15 @@ onMounted(async () => {
         <img
           :src="member.avatar || '/user.png'"
           alt="Avatar"
-          class="w-24 h-24 rounded-full border-2 border-purple-300 object-cover shadow"
+          class="w-24 h-24 lg:w-20 lg:h-20 rounded-full border-2 border-purple-300 object-cover shadow cursor-pointer"
         />
 
         <!-- Nome + Username -->
         <div>
-          <h2 class="text-lg font-semibold text-purple-800 leading-tight mt-4">
+          <h2 class="text-lg lg:text-sm font-semibold text-purple-800 leading-tight mt-4">
             {{ member.name }}
           </h2>
-          <p class="text-sm text-gray-500">@{{ member.user }}</p>
+          <p class="text-sm lg:text-xs text-violet-400">@{{ member.user }}</p>
         </div>
       </div>
 
@@ -49,79 +50,102 @@ onMounted(async () => {
 
       
       <!-- Casa -->
-      <div class="flex flex-col items-center absolute left-6 top-0">
-        <h1 class="font-semibold text-sm uppercase mb-2 text-purple-600">Casa</h1>
+      <div class="flex flex-col items-center absolute left-6 top-20 lg:left-10 lg:top-8">
         <img
           v-if="member.house?.thumb"
           :src="`/houses_flags/${member.house.thumb}`"
           alt="Casa"
-          class="w-8 object-contain rounded-b-full shadow-sm"
+          class="w-8 lg:w-6 object-contain rounded-b-full shadow-sm"
         />
         <div
           v-else
-          class="w-8 h-12 bg-violet-200 rounded-b-full shadow-sm text-purple-600 flex items-center justify-center text-xs rounded"
+          class="w-8 lg:w-6 lg:h-10 h-12 bg-violet-200 rounded-b-full shadow-sm text-purple-600 flex items-center justify-center text-xs rounded"
         >
           —
         </div>
 
-        <p class="text-xs mt-1 font-medium text-purple-600">
+        <p class="text-xs mt-1 lg:text-[10px] font-medium text-purple-600">
           {{ member.house?.name || 'Sem casa' }}
         </p>
 
       </div>
 
-      <!-- Informações principais -->
-      <div class="grid grid-cols-2 text-start gap-2 w-full text-sm text-gray-700 mt-2 ml-2">
-        <div><span class="font-medium text-purple-700">Cargo:</span> 
-          <span 
-            class="font-medium text-violet-700 ml-2"
-          >
+      <div class="grid grid-cols-2 text-start w-full text-sm lg:text-[11px] text-gray-700 mt-4 ml-2">
+        <!-- Cargo -->
+        <div class="flex w-full">
+          <span class="flex items-center gap-1 font-medium text-purple-700">
+            <Lucide icon="BadgeCheck" class="w-3 h-3" />
+            Cargo:
+          </span>
+          <span class="font-medium text-violet-500 ml-2">
             {{ member.role === 'admin' || member.role === 'superadmin' ? 'Adm' : 'Membro' }}
           </span>
         </div>
 
-        <div><span class="font-medium text-purple-700">Ranking:</span> 
-          <span 
-            class="font-medium text-violet-700 ml-2"
-          >
+        <!-- Ranking -->
+        <div class="flex w-full">
+          <span class="flex items-center gap-1 font-medium text-purple-700">
+            <Lucide icon="Trophy" class="w-3 h-3" />
+            Ranking:
+          </span>
+          <span class="font-medium text-violet-500 ml-2">
             {{ member.tier?.fullLabel || 'N/A' }}
           </span>
         </div>
 
-
-        <div><span class="font-medium text-purple-700">Obras:</span> 
-          <span 
-            class="font-medium text-violet-700 ml-2"
-          > 
+        <!-- Obras publicadas -->
+        <div class="flex w-full">
+          <span class="flex items-center gap-1 font-medium text-purple-700">
+            <Lucide icon="BookOpenText" class="w-3 h-3" />
+            Obras:
+          </span>
+          <span class="font-medium text-violet-500 ml-2">
             {{ member.numStoriesPublished }}
           </span>
         </div>
 
-        <div><span class="font-medium text-purple-700">Pontos:</span> 
-          <span 
-            class="font-medium text-violet-700 ml-2"
-          >
+        <!-- Pontos -->
+        <div class="flex w-full">
+          <span class="flex items-center gap-1 font-medium text-purple-700">
+            <Lucide icon="Star" class="w-3 h-3" />
+            Pontos:
+          </span>
+          <span class="font-medium text-violet-500 ml-2">
             {{ member.tierPoints }}
           </span>
         </div>
-        
-        <div><span class="font-medium text-purple-700">Medal:</span>
-          <span 
-            class="font-medium text-violet-700 ml-2"
-          >
+
+        <!-- Medalhas -->
+        <div class="flex w-full">
+          <span class="flex items-center gap-1 font-medium text-purple-700">
+            <Lucide icon="Medal" class="w-3 h-3" />
+            Medalha:
+          </span>
+          <span class="font-medium text-violet-500 ml-2">
             0
           </span>
         </div>
-        <div><span class="font-medium text-purple-700">Conquistas:</span> <span class="font-medium text-violet-700"> 0</span> </div>
+
+        <!-- Conquistas -->
+        <div class="flex w-full">
+          <span class="flex items-center gap-1 font-medium text-purple-700">
+            <Lucide icon="BadgeCheck" class="w-3 h-3" />
+            Conquistas:
+          </span>
+          <span class="font-medium text-violet-500 ml-2">
+            0
+          </span>
+        </div>
       </div>
 
       <!-- Link para o perfil -->
       <a
         :href="member.deeplink"
         target="_blank"
-        class="text-xs font-semibold text-purple-700 hover:underline mt-2"
+        class="flex items-center justify-center gap-1 text-xs font-semibold text-purple-700 hover:underline mt-6"
       >
-        Ver no Wattpad
+      Ver no Wattpad
+      <Lucide icon="ExternalLink" class="w-3 h-3" />
       </a>
     </div>
   </div>
